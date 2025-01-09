a29fcfdcba9e7ad8b5f6e0da5bd8bf4e81d70ef1
Felipe Martins <felipefmartins@gmail.com>
2025-01-09 14:48:38 -0400

    Exercises from chapter 1

---
+++ home_work/ch01/ex01.py
@@ -0,0 +1,25 @@
+# You can use a minus sign to make
+# a negative number like -2.
+# What happens if you put a plus sign
+# before a number? What about ++2?
+
+# If we put a plus sign before a number
+# python will create a literal positive
+# number type (int or float accordingly)
+
+
+# Put several plus and minus operators one
+# after another accumulates and produces
+# a single real number sign
+
+# Two values with no operators between them
+# will throw a interpreter error
+
+def main():
+    print(+3.1415926)
+    print(++2)
+#    print(4 2)
+    print(round(0.5)) 
+
+if __name__ == '__main__':
+    main()
